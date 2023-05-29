da25c3e220808dac98b1f4c63189d973ee65f4e2
jisoo <skscjswo32@gmail.com>
2023-05-29 18:10:29 +0900

    feat admin 화면구성 조정

--- src/page/admin/PetSitterManage.js
+++ src/page/admin/PetSitterManage.js
@@ -1,41 +1,33 @@
-function PetSitterManage() { 
+function PetSitterManage() {
     return (
-        <div>
-        <div>
-            {/* 헤더 */}
-        </div>
-        <div>
-            {/* 메뉴바 */}
-            <div>대시보드</div>
-            <div>회원 관리</div>
-            <div>게시판 관리</div>
-            <div>신고 관리</div>
-        </div>
-        <div>
-            <div>
-                <h2>회원 관리</h2>
+        <div className="container">
+
+            <div className="menuheader">
+                펫시터 모집글 게시판
             </div>
-            <div>
-                <h3>상세검색</h3>
-                <label htmlFor="Writer">작성자 : </label>
-                <input type="text"/>
-                <button>검색</button>
-                <div>
-                    <button>상태 변경</button>
-                </div>
-                <div>
-                    <h4>게시글 번호</h4>
-                    <h4>게시글 제목</h4>
-                    <h4>작성자</h4>
-                    <h4>게시글 등록일</h4> 
-                    <h4>체크</h4>
-                </div>
+            <div className="searchheader2">
+                상세검색
+                <label htmlFor="nickname" className="reporter">작성자 : </label>
+                <input type="text" />
+                <button className="searchbutton2">검색</button>
             </div>
+            <div className="buttonlist">
+                <button className="changebutton">상태 변경</button>
+            </div>
+
+            <table className="memberlist">
+                <thead>
+                    <tr>
+                        <th>게시글 번호</th>
+                        <th>게시글 제목</th>
+                        <th>작성자</th>
+                        <th>게시글 등록일</th>
+                        <th>체크</th>
+                    </tr>
+                </thead>
+            </table>
+
         </div>
-        <div>
-            {/* 푸터 */}
-        </div>
-    </div>
     )
 }
 
--- src/page/admin/ReportMember.js
+++ src/page/admin/ReportMember.js
@@ -1,34 +1,36 @@
-function ReportMember() { 
+import '../../css/admin.css';
+
+function ReportMember() {
     return (
-    <div>
-        <div>
-            <div>
-                <h2>회원 신고</h2>
-                <h2>게시글 신고</h2>
+        <div className="container">
+            <div className="menuheader">
+                회원 신고 게시글 신고
             </div>
-            <div>
-                <h3>상세검색</h3>
-                <label htmlFor="reporter">신고자 : </label>
-                <input type="text"/>
-                <button>검색</button>
-                <div>
-                    <button>상태 변경</button>
-                </div>
-                <div>
-                    <h4>신고 번호</h4>
-                    <h4>신고자</h4>
-                    <h4>피신고자</h4>
-                    <h4>신고일자</h4> 
-                    <h4>신고사유</h4>
-                    <h4>처리상태</h4>
-                    <h4>체크</h4>
-                </div>
+            <div className="searchheader2">
+                상세검색
+                <label htmlFor="nickname" className="reporter">신고자 : </label>
+                <input type="text" />
+                <button className="searchbutton2">검색</button>
             </div>
+            <div className="buttonlist">
+                <button className="changebutton">상태 변경</button>
+            </div>
+
+            <table className="memberlist">
+                <thead>
+                    <tr>
+                        <th>신고 번호</th>
+                        <th>신고자</th>
+                        <th>피신고자</th>
+                        <th>신고일자</th>
+                        <th>신고사유</th>
+                        <th>처리상태</th>
+                        <th>체크</th>
+                    </tr>
+                </thead>
+            </table>
+
         </div>
-        <div>
-            {/* 푸터 */}
-        </div>
-    </div>
     )
 }
 
