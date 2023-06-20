97441ce4c154163f34e46fd3ded9fae2cc02d2c9
DESKTOP-PJLQ947\tjoeun <choemyeonggeon@gmail.com>
2023-06-20 14:27:08 +0900

    feature/myeonggeon

--- src/page/list/PetMomList.js
+++ src/page/list/PetMomList.js
@@ -1,41 +1,106 @@
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
+import React, { useState, useEffect } from 'react';
 import "../../css/petsitterList.css";
+// import sigunguList from '../../data/sigoongu.json';
+import { useDispatch, useSelector } from "react-redux";
 import "../../css/petmomList.css";
 import PageBtn from "../../component/common/PageBtn";
+import { getPetMomList } from "../../api/petMomAPI";
 
+// function searchSig(sido) {
 
+//     return sigunguList.filter(sig => sig.sig.sig_full_nm.startsWith(sido));
+// }    
+
+// function Sigoon({ sig }) {
+
+//     return <option value={sig.sig.sig_kor_nm} >{sig.sig.sig_kor_nm}</option>;
+// }
 function PetMomList() {
 
+    const dispatch = useDispatch();
+    const { data: petmomList, pageInfo } = useSelector(state => state.petMomReducer);
+    const currentPage = useSelector(state => state.pageReducer);
+    const [searchValue, setSearchValue] = useState({})
+
+
+    useEffect(
+        () => {
+            dispatch(getPetMomList(currentPage, searchValue));
+        }
+        , []
+    )
     const navigate = useNavigate()
 
-    // <>
-    //     <h1 >PetMomList</h1>
-    //     <h2 onClick={() => navigate("./1")} >Go PetMomListDetail</h2>
-    //     <h2 onClick={() => navigate("./recruit")} >Go PetMomRecruit</h2>
-    // </>
+
+    const handleItemClick = () => {
+        navigate("./PetMomRecuitDetail");
+    };
+
+    // // 시도 선택시 시군구 리스트 담음
+    // const [sigList, setSigList] = useState([]);
+
+    // // 시도 + 시군구 합치기
+    // const [location, setLocation] = useState({
+    //     SIDO: '',
+    //     SIGUNGU: ''
+    // });
+
+    // const onChangeSidoHandler = (e) => {
+    //     setSigList(searchSig(e.target.value));
+    //     setLocation({
+    //         ...location,
+    //         SIDO: e.target.value
+    //     })
+    // }
+
+    // const onChangeSigunguHandler = (e) => {
+    //     setLocation({
+    //         ...location,
+    //         SIGUNGU: e.target.value
+    //     })
+    // }
+
+
 
     return (
         <div>
-
-
             <button className="write" onClick={() => navigate("./recruit")}>글쓰기</button>
             <br />
             <div className="main">
 
                 <div className="mainpagebox">
                     <div className="wherewhen">
+                        {/* <select className="firstselect" id="sigungu" onChange={onChangeSidoHandler} readOnly>
+                            <option value="">시/도</option>
+                            <option value="서울">서울특별시</option>
+                            <option value="부산">부산광역시</option>
+                            <option value="대구">대구광역시</option>
+                            <option value="인천">인천광역시</option>
+                            <option value="광주">광주광역시</option>
+                            <option value="대전">대전광역시</option>
+                            <option value="울산">울산광역시</option>
+                            <option value="세종특별자치시">세종특별자치시</option>
+                            <option value="경기도">경기도</option>
+                            <option value="강원도">강원도</option>
+                            <option value="충청북도">충청북도</option>
+                            <option value="충청남도">충청남도</option>
+                            <option value="전라북도">전라북도</option>
+                            <option value="전라남도">전라남도</option>
+                            <option value="경상북도">경상북도</option>
+                            <option value="경상남도">경상남도</option>
+                            <option value="제주특별자치도">제주특별자치도</option>
+                        </select>
+
+                        <div className="sidogu">
+                            <select className="secondselect" id="sigungu" onChange={onChangeSigunguHandler} readOnly>
+                                <option value="">시 / 군 / 구</option>
+                                {sigList.map(sig => <Sigoon key={sig.id} sig={sig} />)}
+                            </select>
+
+                        </div> */}
                         <section>
-                            <h4 className="where">어디에 사시나요?</h4>
-                            <div className="sidogu">
-                                <select className="firstselect">
-                                    <option value="">시/도</option>
-                                </select>
-                                <select className="secondselect">
-                                    <option value="">시/군/구</option>
-                                </select>
-                            </div>
-                        </section>
-                        <section>
+
                             <h4 className="when">언제 맡기길 원하시나요?</h4>
                             <div>
                                 <input className="dateselect" type="date" />~<input className="dateselect" type="date" />
@@ -63,38 +128,45 @@ function PetMomList() {
             </div>
 
 
-            <div className="in" >
+            {Array.isArray(petmomList) && petmomList.map(petmom => (
+                <div key={petmom.boardId}>
+                    <div className="in" onClick={handleItemClick} >
 
-                <img className="dogimg" src="../img/home.png"></img>
-                <div className="textlist">
-                    <div className="wheretext">
-                        <div className="dis-flex">경기도 성남시 분당구<div className="asin">→ 애신</div></div>
-                        <div>작성일 2023-05-17</div>
-                    </div>
-                    <div>
-                        <h2>강아지 보는게 뭐 어렵다고</h2>
-                        <hr className="line"></hr>
-                    </div>
-                    <div className="columncss">
-                        <div>단독 주택,반려동물 있어요,노견 케어</div>
-                        <div>기간: 2023-05-18 ~ 2023-05-18</div>
-                        <div className="stardivbtn">
-                            <div className="dis-flex">
-                                <div className="star">★★★★★</div>리뷰 1234개
+                        <img className="dogimg" src="../img/angrydog.png"></img>
+
+                        <div className="textlist">
+                            <div className="wheretext">
+                                <div className="dis-flex">경기도 성남시 분당구</div>
+                                <div>{petmom.boadrDate}</div>
                             </div>
-                            {/* <img className="star" src="../img/star.png"></img> */}
-                            <div className="divbtn flex-column">
-                                <div>50000￦
-                                    <button>1박</button>
-                                </div>
-                                <div>사례￦
-                                    <button>모집중</button>
+                            <div>
+                                <h2>{petmom.boardTitle}</h2>
+                                <hr className="line"></hr>
+                            </div>
+                            <div className="columncss">
+                                <div>{petmom.houseType} {petmom.petYN ? "반려동물있어요" : "반려동물 없어요"} {petmom.care}</div>
+                                <div>기간: {petmom.startDate} ~ {petmom.endDate}</div>
+                                <div className="stardivbtn">
+                                    <div className="dis-flex">
+                                    </div>
+                                    {/* <img className="star" src="../img/star.png"></img> */}
+                                    <div className="divbtn flex-column">
+                                        <div>{petmom.dateRate}￦
+                                            <button>1박</button>
+                                        </div>
+                                        <div>{petmom.hourlyRate}￦
+                                            <button>시간당</button>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
+
                     </div>
+
                 </div>
-            </div>
+
+            ))}
             <PageBtn />
         </div>
     )
