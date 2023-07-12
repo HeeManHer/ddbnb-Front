c4283e9eb7d6180cc43e3fa1e6e0f4d84e4a9dcb
HeeManHer <121776276+HeeManHer@users.noreply.github.com>
2023-07-12 13:29:43 +0900

    Update LoginPage.js

--- src/page/main/LoginPage.js
+++ src/page/main/LoginPage.js
@@ -1,16 +1,16 @@
 import style from './MainStyle.module.css';
 
 const KAKAO_REST_API_KEY = '202bf1013addf514255b52a8c9c69ebf';
-const KAKAO_REDIRECT_URI = `http://www.ddbnb.site/kakao/callback`;
+const KAKAO_REDIRECT_URI = `https://www.ddbnb.site/kakao/callback`;
 const KAKAO_AUTH_URI = `https://kauth.kakao.com/oauth/authorize?client_id=${KAKAO_REST_API_KEY}&redirect_uri=${KAKAO_REDIRECT_URI}&response_type=code`;
 
 const NAVER_CLIENT_ID = 'T0mWG2VjAfBH9cYz6Qrf';
-const NAVER_REDIRECT_URI = encodeURI(`http://www.ddbnb.site/login/oauth2/code/naver`);
+const NAVER_REDIRECT_URI = encodeURI(`https://www.ddbnb.site/login/oauth2/code/naver`);
 const NAVER_STATE_STRING = 'login';
 const NAVER_AUTH_URI = `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${NAVER_CLIENT_ID}&redirect_uri=${NAVER_REDIRECT_URI}&state=${NAVER_STATE_STRING}`;
 
 // const GOOGLE_CLIENT_ID = '525204361597-ih6gpjgmfsa797sinujfj7jgikj0qp0h.apps.googleusercontent.com';
-// const GOOGLE_REDIRECT_URI = encodeURI(`http://www.ddbnb.site/login/oauth2/code/google`);
+// const GOOGLE_REDIRECT_URI = encodeURI(`https://www.ddbnb.site/login/oauth2/code/google`);
 // const GOOGLE_AUTH_URI = `https://accounts.google.com/o/oauth2/auth?client_id=${GOOGLE_CLIENT_ID}&redirect_uri=${GOOGLE_REDIRECT_URI}&response_type=code&scope=openid%20email`;
 
 const loginHandler = (platform) => {
