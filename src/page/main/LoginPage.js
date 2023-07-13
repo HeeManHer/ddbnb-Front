10fe84ad7c81b3089a759ab42f7fa8a908d9c3a7
HeeManHer <121776276+HeeManHer@users.noreply.github.com>
2023-07-13 10:08:55 +0900

    Update LoginPage.js

--- src/page/main/LoginPage.js
+++ src/page/main/LoginPage.js
@@ -1,11 +1,11 @@
 import style from './MainStyle.module.css';
 
 const KAKAO_REST_API_KEY = '202bf1013addf514255b52a8c9c69ebf';
-const KAKAO_REDIRECT_URI = `http://${process.env.REACT_APP_LOGIN_URL}/kakao/callback`;
+const KAKAO_REDIRECT_URI = `https://${process.env.REACT_APP_LOGIN_URL}/kakao/callback`;
 const KAKAO_AUTH_URI = `https://kauth.kakao.com/oauth/authorize?client_id=${KAKAO_REST_API_KEY}&redirect_uri=${KAKAO_REDIRECT_URI}&response_type=code`;
 
 const NAVER_CLIENT_ID = 'T0mWG2VjAfBH9cYz6Qrf';
-const NAVER_REDIRECT_URI = encodeURI(`http://${process.env.REACT_APP_LOGIN_URL}/login/oauth2/code/naver`);
+const NAVER_REDIRECT_URI = encodeURI(`https://${process.env.REACT_APP_LOGIN_URL}/login/oauth2/code/naver`);
 const NAVER_STATE_STRING = 'login';
 const NAVER_AUTH_URI = `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${NAVER_CLIENT_ID}&redirect_uri=${NAVER_REDIRECT_URI}&state=${NAVER_STATE_STRING}`;
 
